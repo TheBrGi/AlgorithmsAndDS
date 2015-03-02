2f1bb7a19d29b2add6210e5dc56a1b15077454c2
TheBrGi <jonnybrgi@gmail.com>
2015-03-02 22:19:40 +0100

    Aggiunto TODO

--- app/src/main/java/com/vinjoe/algorithms/MainActivity.java
+++ app/src/main/java/com/vinjoe/algorithms/MainActivity.java
@@ -1,21 +1,17 @@
 package com.vinjoe.algorithms;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 
@@ -134,7 +130,13 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Bundle args=getArguments();
+            int i=args.getInt(ARG_SECTION_NUMBER);
+            TextView t=(TextView)rootView.findViewById(R.id.section_label);
+            t.setText("Capitolo "+i+"\nArgomento da inserire");//TODO: inserire argomento qui
+
             return rootView;
         }
 
